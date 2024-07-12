81630c7d604111ab5a83fcf7135b80839a25e46e
[Ankith Deegoju] <chanducharan529@gmail.com>
2024-07-12 16:15:37 +0530

    Completed the next.org tutorial.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -7,10 +7,17 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { inter } from 'app/ui/font'
+import { useActionState } from 'react';
+import { authenticate } from '@/app/lib/actions';
 
-export default function LoginForm() {
+export default function LoginForm() 
+{
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined,
+  );
   return (
-    <form className="space-y-3">
+    <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${inter.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -60,7 +67,12 @@ export default function LoginForm() {
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
         </div>
       </div>
     </form>
